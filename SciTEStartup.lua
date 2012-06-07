e35c05c68284cde861bd4760c52b098d08e656fc
Maksim Lin <maks@manichord.com>
2012-06-08 07:41:57 +1000

    better jshint with scite using lua scripting

---
+++ SciTEStartup.lua
@@ -0,0 +1,22 @@
+print ("startup loaded")
+
+function OnSave(filename)
+    if (insave) then
+       return
+    end    
+    --print("on save: "..filename) 
+    local JS_EXT = 'js'
+    local IDM_BUILD = 302 -- from SciTE.h
+    local ext = props['FileExt']  -- e.g 'js'
+    if ext == JS_EXT then
+        local f = io.popen("jshint --reporter /home/maks/config/js/scite-jshint-reporter.js "..filename.." 2>&1", 'r')
+        local s = assert(f:read('*a'))
+        f:close()
+        output:ClearAll()
+        print (s)
+    else
+       --print ("not JS file "..filename)       
+    end
+    
+    return false -- dont swallow the event
+end
